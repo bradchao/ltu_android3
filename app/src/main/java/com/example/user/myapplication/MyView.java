01bad6869dd346dcefb09fe7ad1a6db8e451a015
brad <brad@brad.tw>
2017-11-29 14:54:31 +0800

    201711291454

---
+++ app/src/main/java/com/example/user/myapplication/MyView.java
@@ -0,0 +1,15 @@
+package com.example.user.myapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by User on 2017/11/29.
+ */
+
+public class MyView extends View {
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
