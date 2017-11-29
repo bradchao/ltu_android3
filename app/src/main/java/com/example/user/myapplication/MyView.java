03691205c0f9588926e108e8d8407bcedf6f2aa5
brad <brad@brad.tw>
2017-11-29 15:08:06 +0800

    201711291408

--- app/src/main/java/com/example/user/myapplication/MyView.java
+++ app/src/main/java/com/example/user/myapplication/MyView.java
@@ -1,6 +1,10 @@
 package com.example.user.myapplication;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,7 +13,23 @@ import android.view.View;
  */
 
 public class MyView extends View {
+    private Bitmap ballBmp;
+    private Resources res;
+
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        res = context.getResources();
+
+        ballBmp = BitmapFactory.decodeResource(res, R.drawable.ball);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(ballBmp, 0, 0, null);
+
     }
 }
